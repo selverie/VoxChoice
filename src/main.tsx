0fb2ebb13cb93bdb9e23cf0240822f848fc76a08
selv <130154543+selverie@users.noreply.github.com>
2024-02-22 18:35:32 +0700

    added: new page
    getstarted, signin, signup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.scss';
+import './styles/tailwind.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignUpPage from './views/LoginPage/SignUpPage';
+import SignInPage from './views/LoginPage/SignInPage';
+import HomePage from './views/HomePage/HomePage';
+import GetStarted from './views/GetStarted/GetStarted';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      <Routes>
+        <Route path="/" element={<GetStarted />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+      </Routes>
+    </React.StrictMode>
+  </Router>,
 );
